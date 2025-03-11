200f080b8d4619e6cdc17aa57cfcc329bb1ccb72
kyeong-bin <kyeongb0315@gmail.com>
2025-03-12 02:28:48 +0900

    feat: 문제가 되는 부분 출력 기능

--- week1/main.py
+++ week1/main.py
@@ -4,6 +4,7 @@ try:
     with open('./week1/mission_computer_main.log', 'r') as log_file:
         log_content = log_file.readlines()
 
+    # 로그를 역순으로 정렬한 것을 보고서 형식으로 작성
     sorted_log_content = sorted(log_content[1:], key=lambda x: x.split(',')[0], reverse=True)
 
     markdown_content = "# Log Analysis Report\n\n"
@@ -11,14 +12,28 @@ try:
     markdown_content += "| Timestamp | Event | Message |\n"
     markdown_content += "| --- | --- | --- |\n"
 
+    problematic_markdown_content = "# Problematic Log Entries\n\n"
+    problematic_markdown_content += "| Timestamp | Event | Message |\n"
+    problematic_markdown_content += "| --- | --- | --- |\n"
+
     for line in sorted_log_content:
         timestamp, event, message = line.strip().split(',')
         markdown_content += f"| {timestamp} | {event} | {message} |\n"
 
+        # 문제가 되는 키워드를 저장
+        if 'unstable' in message or 'explosion' in message:
+            problematic_markdown_content += f"| {timestamp} | {event} | {message} |\n"
+
+    # 전체 보고서 저장
     with open('./week1/log_analysis.md', 'w') as markdown_file:
         markdown_file.write(markdown_content)
 
+    # 문제가 되는 부분 따로 저장
+    with open('./week1/problematic_log_entries.md', 'w') as problematic_file:
+        problematic_file.write(problematic_markdown_content)
+
     print('로그 내용을 시간의 역순으로 Markdown 형식으로 성공적으로 변환하고 저장했습니다.')
+    print(f'문제가 되는 로그 엔트리 파일을 저장했습니다.')
 
 except FileNotFoundError:
     print('오류: 파일을 찾을 수 없습니다.')
---
+++ week1/problematic_log_entries.md
@@ -0,0 +1,6 @@
+# Problematic Log Entries
+
+| Timestamp | Event | Message |
+| --- | --- | --- |
+| 2023-08-27 11:40:00 | INFO | Oxygen tank explosion. |
+| 2023-08-27 11:35:00 | INFO | Oxygen tank unstable. |

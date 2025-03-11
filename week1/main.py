432e0b299da3338cf2308afcaea0e4ee6a17a12d
kyeong-bin <kyeongb0315@gmail.com>
2025-03-12 02:07:43 +0900

    feat: 로그 내용 시간의 역순으로 출력

--- week1/main.py
+++ week1/main.py
@@ -4,19 +4,21 @@ try:
     with open('./week1/mission_computer_main.log', 'r') as log_file:
         log_content = log_file.readlines()
 
+    sorted_log_content = sorted(log_content[1:], key=lambda x: x.split(',')[0], reverse=True)
+
     markdown_content = "# Log Analysis Report\n\n"
     markdown_content += "### Log Entries\n\n"
     markdown_content += "| Timestamp | Event | Message |\n"
     markdown_content += "| --- | --- | --- |\n"
 
-    for line in log_content[1:]:
+    for line in sorted_log_content:
         timestamp, event, message = line.strip().split(',')
         markdown_content += f"| {timestamp} | {event} | {message} |\n"
 
     with open('./week1/log_analysis.md', 'w') as markdown_file:
         markdown_file.write(markdown_content)
 
-    print('로그 내용을 Markdown 형식으로 성공적으로 변환하고 저장했습니다.')
+    print('로그 내용을 시간의 역순으로 Markdown 형식으로 성공적으로 변환하고 저장했습니다.')
 
 except FileNotFoundError:
     print('오류: 파일을 찾을 수 없습니다.')
